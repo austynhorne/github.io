372deaa65ae3c416843b0de5587eb7a730034185
austynhorne <austynhorne@byui.edu>
2021-06-04 20:43:34 -0600

    index

---
+++ Lesson7/Gallery Page/JS/Lazyload.js
@@ -0,0 +1,29 @@
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+  img.setAttribute('src',
+  img.getAttribute('data-src'));
+  img.onload =() =>
+  {img.removeAttribute('data-src');};
+
+}
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "290px 0px 290px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  })
+}, imgOptions);
+
+images.forEach(Image => {
+  imgObserver.observe(Image);
+});
---
+++ Lesson7/Gallery Page/JS/Preston_JS.js
@@ -0,0 +1,30 @@
+let date = new Date();
+let day = new Array(7)
+day[0] = "Sunday";
+day[1] = "Monday";
+day[2] = "Tuesday";
+day[3] = "Wednesday";
+day[4] = "Thursday"
+day[5] = "Friday";
+day[6] = "Saturday"
+let month = new Array(12)
+month[0] = "Januray";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+let FullDate = day[date.getDay()] + ", " + " " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+document.querySelector('#date').innerHTML = FullDate;
+if (date.getDay() === 5) {
+    document.querySelector('.banner').style.display = 'block';
+    }
+    else {
+       document.querySelector('.banner').style.display = 'none';
+    }
