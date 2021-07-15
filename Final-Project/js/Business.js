ad0dc0b88f1f2ba207b7bdd8d2e8b1cf607fe7b5
austynhorne <austynhorne@byui.edu>
2021-07-15 00:16:39 -0600

    index

--- Final-Project/js/Business.js
+++ Final-Project/js/Business.js
@@ -2,6 +2,6 @@ fetch('https://austynhorne.github.io/github.io/Final-Project/Business.json')
 .then((response) => response.json())
 .then((jsObject) => {
   console.log(jsObject);
-  document.createElement('name').textContent = jsObject.business[0].businessID_1.name;
-  document.createElement('Address').textContent = jsObject.business[0].businessID_1.Address;
+  document.getElementById('name').textContent = jsObject.Business[0].name;
+  document.getElementById('Address').textContent = jsObject.Business[0].Address;
 });
